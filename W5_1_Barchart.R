3508590f3e5d547d1650dadd1016e672d7799f22
MichaelFL1 <146344703+MichaelFL1@users.noreply.github.com>
2024-03-18 16:04:13 +0800

    New bar chart

--- W5_1_Barchart.R
+++ W5_1_Barchart.R
@@ -1,9 +1,17 @@
 install.packages("reshape2")
 require(ggplot2)
 require(reshape2)
+library(tidyr)
 
 #Read csv file and transform
 data <- read.csv("/Users/michaelfitolimardi/Documents/Viz2/Accidents.csv")
+#Convert to longer format
+accidents_longer <- data %>% pivot_longer(cols=c('Death','Injured'),
+                                              names_to='Type',
+                                              values_to='Count')
+accidents_longer
+#ggplot(accidents_longer, aes(x=Year, y=value, fill=variable))+geom_bar()#+labs(title="Michael, Bar Chart")+theme(plot.title=element_text(hjust=1, size=35))
+
 
 data
 df <- melt(data, id = "Year")
